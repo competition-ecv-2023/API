d1f8d83897baf71820ef8b60966c50a27f5f2064
Dancuo-Lohan <dancuo.lohan@gmail.com>
2023-06-14 15:35:25 +0200

    Add token API verification.

--- v1/config/settings.php
+++ v1/config/settings.php
@@ -7,6 +7,7 @@ $dbHost = 'localhost';
 $dbName = 'ecvapi';
 $dbUser = 'root';
 $dbPassword = '';
+$apiToken = 'ZKqZfeQZtJpXWuBe3T4HBr5FSWFdc6zhQPmeZDrEtQBUnxSxgDqNRSAw3CzSEyu8FPt6aNgHrVj4j7tfyGpLwGzm3sgz8qLdCTykHYEaGJm3ykGvPG5qznaK';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
--- v1/index.php
+++ v1/index.php
@@ -5,15 +5,18 @@ require 'v1/config/settings.php';
 use Api\ApiHandler;
 
 $request_method = $_SERVER["REQUEST_METHOD"]; //GET, POST, PUT, DELETE, etc...
+$headerToken = isset($_SERVER['HTTP_AUTHORIZATION']) ? $_SERVER['HTTP_AUTHORIZATION'] : '';
 $allowed_methods = ["GET", "POST", "PUT", "DELETE"];
 
-if (isset($_GET) && !empty($_GET)) {
-    if (isset($_GET["page"]) && $_GET['page'] == "swagger") {
-        if($_GET['page'] == "swagger") {
-        }
-    } else if (isset($_GET["api"]) && in_array($request_method, $allowed_methods)) {
-        if(file_exists($_SERVER['DOCUMENT_ROOT'].'/v1/routes/'.$request_method.'/'.$_GET['api'].'.php')) {
-            require $_SERVER['DOCUMENT_ROOT'].'/v1/routes/'.$request_method.'/'.$_GET['api'].'.php';
+if ($apiToken === $headerToken) {
+    if (isset($_GET) && !empty($_GET)) {
+        if (isset($_GET["page"]) && $_GET['page'] == "swagger") {
+            if($_GET['page'] == "swagger") {
+            }
+        } else if (isset($_GET["api"]) && in_array($request_method, $allowed_methods)) {
+            if(file_exists($_SERVER['DOCUMENT_ROOT'].'/v1/routes/'.$request_method.'/'.$_GET['api'].'.php')) {
+                require $_SERVER['DOCUMENT_ROOT'].'/v1/routes/'.$request_method.'/'.$_GET['api'].'.php';
+            }
         }
     }
 }
