29a8a045b5592208ef197a971a53f167ec40a24e
Dancuo-Lohan <dancuo.lohan@gmail.com>
2023-06-14 17:10:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 define('ENVIRONMENT', 'development');
 // define('ENVIRONMENT', 'production');
-
+// $_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT']."/patperdue";
 require("v1/index.php");
