2e589bfd565abfa0bef73cb3cba742dcde38fba0
Dancuo-Lohan <dancuo.lohan@gmail.com>
2023-06-15 12:01:49 +0200

    Update login.php

--- v1/routes/POST/users/login.php
+++ v1/routes/POST/users/login.php
@@ -27,7 +27,7 @@ if (isset($_POST['email']) && isset($_POST['passwordToVerify'])) {
             // User now logged
             header("HTTP/1.1 200 OK");
             header("X-Message: Connecte");
-            echo '[{"userToken":"'.$_SESSION["user"]["token"].',"id":'.$userId.'}]';
+            echo '[{"userToken":"'.$_SESSION["user"]["token"].'","id":'.$userId.'}]';
             break;
         case 1:
             // Error with the SQL
