0b011e085afb9bd541682059556aa6c40568fc65
Dancuo-Lohan <dancuo.lohan@gmail.com>
2023-06-20 15:12:02 +0200

    Update adverts.php

--- v1/routes/GET/adverts.php
+++ v1/routes/GET/adverts.php
@@ -14,7 +14,16 @@ if (isset($_GET['advert_id'])) {
     echo json_encode(SQLManager::findBy('*','adverts','id = :id', array(':id' => $_GET['advert_id'])));
 } else if (!isset($_GET['page'])) {
     // Récupère les 10 derniers ID
-    echo json_encode(SQLManager::findAll('*','adverts WHERE end_date >= NOW() AND is_deleted = 0 ORDER BY id DESC LIMIT 10'));
+    $select = "a.*, JSON_ARRAYAGG(i.link) AS advert_images";
+    $from = "adverts AS a
+    JOIN adverts_images AS ai ON a.id = ai.advert_id
+    JOIN images AS i ON ai.image_id = i.id
+    WHERE a.end_date >= NOW() 
+    AND a.is_deleted = 0
+    GROUP BY a.id
+    ORDER BY a.id DESC
+    LIMIT 10;";
+    echo json_encode(SQLManager::findAll($select,$from));
 } else if (intval($_GET['page'])) {
     // Récupère les 10 x $_GET['page'] sans prendre ceux des pages précédentes
     $page_number = ($_GET['page'] - 1) * 10;
